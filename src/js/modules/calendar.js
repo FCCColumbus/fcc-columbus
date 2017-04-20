0bf35dcb7abfae7fdf7c0c611511f8f7b500dd7e
DanBuddy <ds321609@gmail.com>
2017-04-20 16:00:26 -0400

    refactored calendar script

--- src/js/modules/calendar.js
+++ src/js/modules/calendar.js
@@ -1,14 +1,17 @@
 FCCC.calendar = (function(FCCC, $){
-  var key = 'enterappkey';
-  var apiHeaders = new Headers();
-  apiHeaders.append("Authorization", "Bearer " + key);
+  var eventList, name, date, dateFormat, url, setHeaders, apiRequest,
+  key = 'placeapikey',
+  $cEvent = $('.c-events'),
+  apiHeaders = new Headers(),
+  rootAddress = 'https://www.eventbriteapi.com/v3/users/me/events/?status=live';
 
-  var rootAddress = 'https://www.eventbriteapi.com/v3/users/me/events/?status=live';
-  var setHeaders = { headers: apiHeaders };
-  var apiRequest = new Request(rootAddress, setHeaders);
+  apiHeaders.append("Authorization", "Bearer " + key);
+  setHeaders = { headers: apiHeaders };
+  apiRequest = new Request(rootAddress, setHeaders);
+
+
 
   function fetchEvents()  {
-    var eventList;
 
     fetch(apiRequest)
     .then(function(res, err) {
@@ -19,15 +22,15 @@ FCCC.calendar = (function(FCCC, $){
       return body.events;
     }).then(function(events) {
       eventList = events.map(function(event) {
-        var name = event.name.text;
-        var date = new Date(event.start.local);
-        var dateFormat = (date.getMonth() + 1) + "/" + date.getDate();
-        var url = event.url;
+        name = event.name.text;
+        date = new Date(event.start.local);
+        dateFormat = (date.getMonth() + 1) + "/" + date.getDate();
+        url = event.url;
 
         return '<div class="events__card"><a class="clickable" target="_blank" href="' + url + '"></a>' + '<div class="events__card__description"><h3 class="events__card__date">' +
                 dateFormat + '</h3>' + '<h1 class="events__card__title">' + name + '</h1></div></div>';
       });
-      $('.c-events').append(eventList);
+      $cEvent.append(eventList);
     });
   }
 
