4f12a99f6e16afbbc7b0131a388ffd2d54486aed
Justin Woodward <justinwoodward92@gmail.com>
2017-09-29 23:01:01 -0400

    TDD initial test now integrated

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,6 +1,4 @@
 // import axios from 'axios'
 // import MockAdapter from 'axios-mock-adapter'
 
-export const sum = (a, b) => (
-  a + b
-)
+export const TDD = true
--- app/helpers/api.test.js
+++ app/helpers/api.test.js
@@ -1,5 +1,5 @@
-import { sum } from './api'
+import { TDD } from './api'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+test('TDD should equal true', () => {
+  expect(TDD).toBe(true)
 })
