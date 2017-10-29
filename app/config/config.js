11ef02a1192eebe04a9998bddbfd4d1dda91c787
Justin Woodward <justinwoodward92@gmail.com>
2017-10-29 11:14:01 -0400

    API endpoint is now updated to point to a universal URL

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,3 @@
 export const WDS_PORT = process.env.PORT || 4000
-export const calendarAPI = 'https://calendar-microservice-pueuniftds.now.sh/api/calendar'
+export const calendarAPI = 'https://fccc.now.sh/api/calendar'
 // export const calendarAPI = 'https://fccc-calendar-ms.herokuapp.com/api/calendar'
