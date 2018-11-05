b163fc83ed165464ae1428791d0f61e36286f113
Ty Day <tyrday@gmail.com>
2018-11-04 20:52:55 -0500

    Useful links now link to external websites
    replaced <Link> with <a> tags per this discussion:
    https://github.com/ReactTraining/react-router/issues/1147

--- app/components/Resources/Resources.js
+++ app/components/Resources/Resources.js
@@ -9,85 +9,85 @@ const Resources = () => (
         <h2>Useful Links</h2>
       </div>
       <div className={styles.item}>
-        <Link to="https://www.facebook.com/groups/free.code.camp.columbus/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/groups/free.code.camp.columbus/" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-facebook-square`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Facebook Group</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="https://www.twitter.com/fcccolumbus" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.twitter.com/fcccolumbus" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-twitter`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Twitter</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="https://www.freecodecamp.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.freecodecamp.com/" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-free-code-camp`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Free Code Camp</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="http://forum.freecodecamp.com/" target="_blank" rel="noopener noreferrer">
+        <a href="http://forum.freecodecamp.com/" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-comments-o`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>FCC Forum</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="https://www.twitch.tv/fcccolumbus" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.twitch.tv/fcccolumbus" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-twitch`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Twitch Channel</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="https://www.youtube.com/channel/UC6qdwVNZzr0dkQEF7SdsoAg" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.youtube.com/channel/UC6qdwVNZzr0dkQEF7SdsoAg" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-youtube`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Youtube Channel</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link to="https://github.com/FCCColumbus" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/FCCColumbus" target="_blank" rel="noopener noreferrer">
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-github`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Group GitHub</h3>
         </div>
       </div>
       <div className={styles.item}>
-        <Link
-          to="https://drive.google.com/folderview?id=0B52DpJWs0V6GVjl5OS1SdnZ3T0E&usp=sharing"
+        <a
+          href="https://drive.google.com/folderview?id=0B52DpJWs0V6GVjl5OS1SdnZ3T0E&usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
           <div className={styles.logo}>
             <i className={`${styles.icon} fa fa-fw fa-book`} />
           </div>
-        </Link>
+        </a>
         <div className={styles.name}>
           <h3>Online Library</h3>
         </div>
