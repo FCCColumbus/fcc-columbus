88ffc4373b2ee1a7c2df53b5d9d9a0dc1c244a04
Justin Woodward <justinwoodward92@gmail.com>
2019-02-07 21:21:05 -0500

    Production Build (#98)
    * Patch/GH-76: adjusting spacing for calendar title desktop and mobile (#77)
    
    * adjusted spacing and width of image
    
    * added a description of Slack
    
    * Swapping breakpoints to variables
    
    * changed mobile hero title size and spacing
    
    * Slack accessibility and global visuallyhidden class (#87)
    
    Relates to #42
    
    * fixed position default header height (#89)
    
    * Reduce size of resource icons and text (#91)
    
    * Reduce size of resource icons and text
    
    Changed h2 to h3. Reduced width from 50% to 25% to allow for better flow.
    
    * Useful links now link to external websites
    
    replaced <Link> with <a> tags per this discussion:
    https://github.com/ReactTraining/react-router/issues/1147
    
    * Fix links to admin profiles
    
    * Remove unused { Link } import
    
    * Remove youtube and twitch links from Resources.js
    
    * Resolves #90
    
    * Resolves #92
    
    * Patch: calendar-link-to-anchor-tag (#97)
    
    * Patch: calendar-link-to-anchor-tag
    
    * npm audit fixes
    
    * Iss18 Extends readme.md (#94)
    
    * Extend README.md to include github instructions
    
    * fix readmenoobs
    
    * add links to Git and Travis CI lectures

--- app/components/Slack/Slack.js
+++ app/components/Slack/Slack.js
@@ -71,6 +71,11 @@ const Slack = () => {
             Join <span className={styles.bold}>FreeCodeCamp Columbus</span> on Slack.
           </h1>
           <GetUsers />
+          <p className={styles.slackDescription}>
+            {
+              "Slack is a free chat client used by lots of tech industry businesses, so it's a good idea to get familiar with it while you're in the process of learning to code. It's great for developers because it has features to make sharing code easy. Our group uses Slack to ask and answer coding questions, showcase projects, share jobs, advertise our events, and just chat. Slack will help you connect with other Free Code Camp Columbus members quickly and easily."
+            }
+          </p>
           <form
             className={styles.innerContainer}
             onSubmit={(e) => {
@@ -81,17 +86,19 @@ const Slack = () => {
             }}
           >
             <label htmlFor="email" className={styles.label}>
+              <span className={styles.visuallyhidden}>Email: </span>
               <input
                 id="email"
                 name="email"
                 className={styles.input}
-                type="text"
+                type="email"
                 placeholder="you@yourdomain.com"
                 autoComplete="off"
                 ref={(node) => (emailInput = handleInputRef(node, data))}
               />
             </label>
             <label htmlFor="name" className={styles.label}>
+              <span className={styles.visuallyhidden}>First Name: </span>
               <input
                 id="firstName"
                 name="name"
